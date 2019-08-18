5f860676591fd84910ba809f6d402f8d63c87fa8
Edson Bastos <edsonbastos2@hotmail.com>
2019-08-18 20:56:32 -0300

    Exemplo de CRUD em nodejs

---
+++ src/servidor.js
@@ -0,0 +1,43 @@
+const porta = 3003;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados');
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/produtos', (req,res, next) => {
+    res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req,res,next) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req,res,next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco,
+        fabricante: req.body.fabricante
+    })
+    res.send(produto); //JSON
+});
+
+app.put('/produtos/:id', (req,res,next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco,
+        fabricante: req.body.fabricante
+    })
+    res.send(produto); //JSON
+});
+
+app.delete('/produtos/:id', (req,res) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id);
+    res.send(produto);
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor sendo execultado na porta ${porta}`)
+});
